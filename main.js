e0cf12d19761ecd45cd76c73522dab21e96fa7bd
Papakhina Irina <papakhina.irina@mail.ru>
2022-10-09 00:34:08 +0300

    ability of search product and delete product

--- main.js
+++ main.js
@@ -1,84 +1,116 @@
-if(!window.localStorage.getItem('goods')) {
-  window.localStorage.setItem('goods', JSON.stringify([]))
-}
-let myModal = new bootstrap.Modal(document.getElementById('exampleModal'),{
+var options = {
+  valueNames: [ 'name', 'price' ]
+};
+var userList;
+var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
   keyboard: false
-})
+});
 
+if(!localStorage.getItem('goods')) {
+  localStorage.setItem('goods', JSON.stringify([]))
+};
 
-document.querySelector('button.add_new').addEventListener('click', function(e){
-  let name = document.getElementById('good_name').value
-  let price = document.getElementById('good_price').value
-  let count = +document.getElementById('good_count').value
-  
+document.querySelector('button.add_new').addEventListener('click', function(e) {
+  let name = document.getElementById('good_name').value;
+  let price = document.getElementById('good_price').value;
+  let count = +document.getElementById('good_count').value;
   if(name && price && count) {
-    document.getElementById('good_name').value = ''
-    document.getElementById('good_price').value = ''
-    document.getElementById('good_count').value = '1'
+    document.getElementById('good_name').value = '';
+    document.getElementById('good_price').value = '';
+    document.getElementById('good_count').value = '1';
+    let goods = JSON.parse(localStorage.getItem('goods'));
+    goods.push(['good_'+goods.length, name, price, count, 0, 0, 0]);
+    localStorage.setItem('goods', JSON.stringify(goods));
     
-    let goods = JSON.parse(window.localStorage.getItem('goods'))
-    goods.push(['good_'+goods.length, name, price, count , 0, 0])
-    window.localStorage.setItem('goods', JSON.stringify(goods))
+    update_goods();
     
-    update_goods()
-    
-    myModal.hide()
-  }else{
+    myModal.hide();
+  } else {
     Swal.fire({
       icon: 'error',
       title: 'Ошибка',
-      text: 'Пожалуйста заполните все поля!'
+      text: 'Пожалуйста заполните все поля!',
     })
   }
-})
+});
 
-update_goods()
+update_goods();
 
-function update_goods(){
-  let result_price = 0
-  let tbody = document.querySelector('.list')
-  tbody.innerHTML = ''
-  document.querySelector('.cart').innerHTML = ''
-  let goods = JSON.parse(window.localStorage.getItem('goods'))
+function update_goods() {
+  let result_price = 0;
+  let tbody = document.querySelector('.list');
+  tbody.innerHTML = "";
+  document.querySelector('.cart').innerHTML = "";
+  let goods = JSON.parse(localStorage.getItem('goods'));
   if(goods.length) {
-    table1.hidden = false
+    table1.hidden = false,
     table2.hidden = false
-    for ( let i = 0; i<goods.length; i++){
+    for(let i=0; i<goods.length; i++) {
       tbody.insertAdjacentHTML('beforeend',
       `
-      <tr class='align-middle'>
-        <td>${i+1}</td>
-        <td class='name'>${goods[i][1]}</td>
-        <td class='price'>${goods[i][2]}</td>
-        <td>${goods[i][3]}</td>
-        <td><button class='good_delete btn-danger' data-delete='${goods[i][0]}'>&#10006;</button></td>
-        <td><button class='good_delete btn-danger' data-goods='${goods[i][0]}'>&#10149;</button></td>
+      <tr class="align-middle">
+      <td>${i+1}</td>
+      <td class="name">${goods[i][1]}</td>
+      <td class="price">${goods[i][2]}</td>
+      <td>${goods[i][3]}</td>
+      <td><button class="good_delete btn-danger" data-delete="${goods[i][0]}">&#10006;</button></td>
+      <td><button class="good_delete btn-primary" data-goods="${goods[i][0]}">&#10149;</button></td>
       </tr>
-      `
-      )
-      if(goods[i][4] > 0) {
-        goods[i][6] = goods[i][4] * goods[i][2] - goods[i][4]*goods[i][2]*goods[i][5]*0.01
+      `)
+      if(goods[i][4]>0) {
+        goods[i][6] = goods[i][4]*goods[i][2] - goods[i][4]*goods[i][2]*goods[i][5]*0.01
         result_price += goods[i][6]
         document.querySelector('.cart').insertAdjacentHTML('beforeend',
-`  
-      <tr class='align-middle'>
+        `
+        <tr class="align-middle">
         <td>${i+1}</td>
-        <td class='price_name'>${goods[i][1]}</td>
-        <td class='price_one'>${goods[i][2]}</td>
-        <td class='price_count'>${goods[i][4]}</td>
-        <td class='price_discount'><input data-goodid='${goods[i][0]}' type='text' value='${goods[i][5]}' min='0' max='100'</td>
+        <td class="price_name">${goods[i][1]}</td>
+        <td class="price_one">${goods[i][2]}</td>
+        <td class="price_count">${goods[i][4]}</td>
+        <td class="price_discount"><input data-goodid="${goods[i][0]}" type="text" value="${goods[i][5]}" min="0" max="100"></td>
         <td>${goods[i][6]}</td>
-        <td><button class='good_delete btn-danger' data-delete='${goods[i][0]}'>&#10006;</button></td>
-        <td><button class='good_delete btn-danger' data-goods='${goods[i][0]}'>&#10149;</button></td>
-      </tr>
-      ` 
-        )
-      }
-    }
-    //userList = new List ('goods', options);
-  }else{
+        <td><button class="good_delete btn-danger" data-delete="${goods[i][0]}">&#10006;</button></td>
+        </tr>
+        `)
+      };
+    };
+    userList = new List('goods', options);
+  } else {
     table1.hidden = true
     table2.hidden = true
-  }
+  };
   document.querySelector('.price_result').innerHTML = result_price + ' &#8381;'
-}
+};
+
+document.querySelector('.list').addEventListener('click', function(e) {  
+  if(!e.target.dataset.delete) {
+    return
+  };
+  Swal.fire({
+    title: 'Внимание!',
+    text: "Вы действительно хотите удалить товар?",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Да',
+    cancelButtonText: 'Отмена',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      let goods = JSON.parse(localStorage.getItem('goods'));
+      for(let i=0; i<goods.length; i++) {
+        if(goods[i][0] == e.target.dataset.delete) {
+          goods.splice(i,1), 
+          localStorage.setItem('goods', JSON.stringify(goods)),
+          update_goods()
+        }
+      };
+      Swal.fire(
+        'Удалено!',
+        'Выбраный товар был успешно удален.',
+        'success'
+        )
+      }
+    })
+  })
+  
