413485a20abb7f7af47c02bd1ce9526c6f0fc506
Papakhina Irina <papakhina.irina@mail.ru>
2022-10-10 17:08:41 +0300

    ability to sort and add discount

--- main.js
+++ main.js
@@ -1,14 +1,49 @@
-var options = {
-  valueNames: [ 'name', 'price' ]
+table1.onclick = function (e) {
+  if (e.target.tagName != 'TH') return;
+  let th = e.target;
+  sortTable(th.cellIndex, th.dataset.type, 'table1');
 };
-var userList;
-var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+
+table2.onclick = function (e) {
+  if (e.target.tagName != 'TH') return;
+  let th = e.target;
+  sortTable(th.cellIndex, th.dataset.type, 'table2');
+};
+
+function sortTable(colNum, type, id) {
+  let elem = document.getElementById(id)
+  let tbody = elem.querySelector('tbody');
+  let rowsArray = Array.from(tbody.rows);
+  let compare;
+  switch (type) {
+    case 'number':
+      compare = function (rowA, rowB) {
+        return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
+      };
+      break;
+    case 'string':
+      compare = function (rowA, rowB) {
+        return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? 1 : -1;
+      };
+      break;
+  }
+  rowsArray.sort(compare);
+  tbody.append(...rowsArray);
+}
+
+  if(!localStorage.getItem('goods')) {
+    localStorage.setItem('goods', JSON.stringify([]))
+  };
+
+let myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
   keyboard: false
 });
 
-if(!localStorage.getItem('goods')) {
-  localStorage.setItem('goods', JSON.stringify([]))
+let options = {
+  valueNames: [ 'name', 'price']
 };
+
+let userList;
 
 document.querySelector('button.add_new').addEventListener('click', function(e) {
   let name = document.getElementById('good_name').value;
@@ -29,7 +64,7 @@ document.querySelector('button.add_new').addEventListener('click', function(e) {
     Swal.fire({
       icon: 'error',
       title: 'Ошибка',
-      text: 'Пожалуйста заполните все поля!',
+      text: 'Пожалуйста заполните все поля!'
     })
   }
 });
@@ -74,7 +109,8 @@ function update_goods() {
         `)
       };
     };
-    userList = new List('goods', options);
+
+  let userList = ('goods', options)
   } else {
     table1.hidden = true
     table2.hidden = true
@@ -94,14 +130,14 @@ document.querySelector('.list').addEventListener('click', function(e) {
     confirmButtonColor: '#3085d6',
     cancelButtonColor: '#d33',
     confirmButtonText: 'Да',
-    cancelButtonText: 'Отмена',
+    cancelButtonText: 'Отмена'
   }).then((result) => {
     if (result.isConfirmed) {
-      let goods = JSON.parse(localStorage.getItem('goods'));
+      let goods = JSON.parse(localStorage.getItem('goods'))
       for(let i=0; i<goods.length; i++) {
         if(goods[i][0] == e.target.dataset.delete) {
-          goods.splice(i,1), 
-          localStorage.setItem('goods', JSON.stringify(goods)),
+          goods.splice(i,1)
+          localStorage.setItem('goods', JSON.stringify(goods))
           update_goods()
         }
       };
@@ -114,3 +150,52 @@ document.querySelector('.list').addEventListener('click', function(e) {
     })
   })
   
+document.querySelector('.list').addEventListener('click', function(e) { 
+  if(!e.target.dataset.goods) {
+    return 
+  }
+  let goods = JSON.parse(localStorage.getItem('goods'))
+    for(let i=0; i<goods.length; i++) {
+      if(goods[i][3] > 0 && goods[i][0] ==e.target.dataset.goods){
+        goods[i].splice(3,1, goods[i][3]-1)
+        goods[i].splice(4,1, goods[i][4]+1)
+        localStorage.setItem('goods', JSON.stringify(goods))
+        update_goods()
+      }
+    }
+}) 
+
+document.querySelector('.cart').addEventListener('click', function(e) { 
+  if(!e.target.dataset.delete) {
+    return 
+  }
+  let goods = JSON.parse(localStorage.getItem('goods'))
+    for(let i=0; i<goods.length; i++) {
+      if(goods[i][4] > 0 && goods[i][0] ==e.target.dataset.delete){
+        goods[i].splice(3,1, goods[i][3]+1)
+        goods[i].splice(4,1, goods[i][4]-1)
+        localStorage.setItem('goods', JSON.stringify(goods))
+        update_goods()
+      }
+    }
+}) 
+
+
+
+document.querySelector('.cart').addEventListener('input', function(e) { 
+  if(!e.target.dataset.goodid) {
+    return 
+  }
+  let goods = JSON.parse(localStorage.getItem('goods'))
+  for(let i=0; i<goods.length; i++) {
+    if(goods[i][0] == e.target.dataset.goodid){
+      goods[i][5] = e.target.value
+      goods[i][6] = goods[i][4]*goods[i][2] - goods[i][4]*goods[i][2]*goods[i][5]*0.01
+      localStorage.setItem('goods', JSON.stringify(goods))
+      update_goods()
+      let input = document.querySelector(`[data-goodid="${goods[i][0]}"]`)
+      input.focus()
+      input.selectionStart = input.value.length
+    }
+  }
+})
